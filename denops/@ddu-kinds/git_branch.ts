359b027b41a64ed68170a85ef5a0f29592877d1e
tennashi <tennashio@gmail.com>
2023-02-23 22:16:41 +0900

    Add git_branch kind.

---
+++ denops/@ddu-kinds/git_branch.ts
@@ -0,0 +1,32 @@
+import { BaseKind, ActionArguments, ActionFlags } from "https://deno.land/x/ddu_vim@v2.3.0/types.ts";
+
+export type ActionData = {
+  branch: string;
+};
+
+type Params = Record<never, never>;
+
+export class Kind extends BaseKind<Params> {
+  actions: Record<string, (args: ActionArguments<Params>) => Promise<ActionFlags>> = {
+    switch: (args: ActionArguments<Params>) => {
+      const decoder = new TextDecoder();
+
+      for (const item of args.items) {
+        const action = item?.action as ActionData;
+
+        const cmd = new Deno.Command("git", { args: ["switch", action.branch] });
+        const result = cmd.outputSync();
+
+        if (!result.success) {
+          console.log(decoder.decode(result.stderr));
+        }
+      }
+
+      return Promise.resolve(ActionFlags.None);
+    },
+  }
+
+  params(): Params {
+    return {};
+  }
+}
