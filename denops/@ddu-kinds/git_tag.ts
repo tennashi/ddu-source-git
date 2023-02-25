7234747c07399bce723401ed719368289287dd8e
tennashi <tennashio@gmail.com>
2023-02-26 02:25:30 +0900

    Add git_tag.create action.

--- denops/@ddu-kinds/git_tag.ts
+++ denops/@ddu-kinds/git_tag.ts
@@ -1,4 +1,5 @@
 import { BaseKind, ActionArguments, ActionFlags } from "https://deno.land/x/ddu_vim@v2.3.0/types.ts";
+import { input } from "https://deno.land/x/denops_std@v4.0.0/helper/mod.ts";
 
 export type ActionData = {
   tag: string;
@@ -6,11 +7,11 @@ export type ActionData = {
 
 type Params = Record<never, never>;
 
+const decoder = new TextDecoder();
+
 export class Kind extends BaseKind<Params> {
   actions: Record<string, (args: ActionArguments<Params>) => Promise<ActionFlags>> = {
     switch: async (args: ActionArguments<Params>): Promise<ActionFlags> => {
-      const decoder = new TextDecoder();
-
       const getCwdResult = await args.denops.call("getcwd")
       const cwd = getCwdResult as string
 
@@ -26,6 +27,28 @@ export class Kind extends BaseKind<Params> {
       }
 
       return ActionFlags.None;
+    },
+
+    create: async (args: ActionArguments<Params>): Promise<ActionFlags> => {
+      const tagName = await input(args.denops, {
+        prompt: "(tag name)> ",
+      });
+
+      const getCwdResult = await args.denops.call("getcwd");
+      const cwd = getCwdResult as string;
+
+      if (!tagName) {
+        return ActionFlags.Persist
+      }
+
+      const cmd = new Deno.Command("git", { args: ["tag", tagName], cwd: cwd });
+      const result = cmd.outputSync();
+
+      if (!result.success) {
+        console.log(decoder.decode(result.stderr));
+      }
+
+      return ActionFlags.RefreshItems;
     },
   }
 
