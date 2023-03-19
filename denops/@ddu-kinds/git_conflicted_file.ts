ec9d96eda4b8315a379f2da910b7b2b82335c05f
tennashi <tennashio@gmail.com>
2023-03-19 18:59:20 +0900

    Use inheritance.

--- denops/@ddu-kinds/git_conflicted_file.ts
+++ denops/@ddu-kinds/git_conflicted_file.ts
@@ -1,8 +1,8 @@
 import {
   ActionArguments,
   ActionFlags,
-  BaseKind,
 } from "https://deno.land/x/ddu_vim@v2.3.0/types.ts";
+import { Kind as WorkingTreeKind } from "./git_working_tree.ts";
 
 export type ActionData = {
   path: string;
@@ -12,64 +12,25 @@ type Params = Record<never, never>;
 
 const decoder = new TextDecoder();
 
-export class Kind extends BaseKind<Params> {
-  actions: Record<
-    string,
-    (args: ActionArguments<Params>) => Promise<ActionFlags>
-  > = {
-    ginChaperon: (args: ActionArguments<Params>): Promise<ActionFlags> => {
-      for (const item of args.items) {
-        const action = item?.action as ActionData;
+export class Kind extends WorkingTreeKind {
+  constructor() {
+    super();
 
-        args.denops.cmd(`:GinChaperon ${action.path}`)
-      }
+    this.actions = {
+      ginChaperon: (args: ActionArguments<Params>): Promise<ActionFlags> => {
+        for (const item of args.items) {
+          const action = item?.action as ActionData;
 
-      return Promise.resolve(ActionFlags.None);
-    },
-    add: async (args: ActionArguments<Params>): Promise<ActionFlags> => {
-      const getCwdResult = await args.denops.call("getcwd");
-      const cwd = getCwdResult as string;
-
-      for (const item of args.items) {
-        const action = item?.action as ActionData;
-
-        const cmd = new Deno.Command("git", {
-          args: ["add", action.path],
-          cwd: cwd,
-        });
-        const result = cmd.outputSync();
-
-        if (!result.success) {
-          console.log(decoder.decode(result.stderr));
+          args.denops.cmd(`:GinChaperon ${action.path}`);
         }
-      }
 
-      return ActionFlags.RefreshItems;
-    },
-    restore: async (args: ActionArguments<Params>): Promise<ActionFlags> => {
-      const getCwdResult = await args.denops.call("getcwd");
-      const cwd = getCwdResult as string;
-
-      for (const item of args.items) {
-        const action = item?.action as ActionData;
-
-        const cmd = new Deno.Command("git", {
-          args: ["restore", action.path],
-          cwd: cwd,
-        });
-        const result = cmd.outputSync();
-
-        if (!result.success) {
-          console.log(decoder.decode(result.stderr));
-        }
-      }
-
-      return ActionFlags.RefreshItems;
-    },
-  };
+        return Promise.resolve(ActionFlags.None);
+      },
+      ...this.actions,
+    };
+  }
 
   params(): Params {
     return {};
   }
 }
-
