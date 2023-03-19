9935cff97f2de79ec5f0e561bf36ef7fdbcf6073
tennashi <tennashio@gmail.com>
2023-03-19 19:53:54 +0900

    Extract highlighting process.

---
+++ denops/@ddu-filters/git_ref_highlight.ts
@@ -0,0 +1,59 @@
+import {
+  BaseFilter,
+  DduItem,
+} from "https://deno.land/x/ddu_vim@v2.4.0/types.ts";
+import { FilterArguments } from "https://deno.land/x/ddu_vim@v2.4.0/base/filter.ts";
+import { ActionData as GitBranchActionData } from "../@ddu-kinds/git_branch.ts";
+import { ActionData as GitTagActionData } from "../@ddu-kinds/git_tag.ts";
+
+type Params = Record<never, never>;
+
+const encoder = new TextEncoder();
+
+function byteLength(input: string): number {
+  return encoder.encode(input).length;
+}
+
+export class Filter extends BaseFilter<Params> {
+  filter(args: FilterArguments<Params>): Promise<DduItem[]> {
+    return Promise.resolve(args.items.map((item: DduItem): DduItem => {
+      const kind = item.kind || "git_branch";
+      switch (kind) {
+        case "git_branch": {
+          const action = item.action as GitBranchActionData;
+          if (action.isRemote) {
+            item.highlights = [
+              {
+                name: "remote",
+                hl_group: "Identifier",
+                col: 1,
+                width: byteLength(action.branch),
+              },
+            ];
+          }
+          break;
+        }
+        case "git_tag": {
+          const action = item.action as GitTagActionData;
+          item.highlights = [
+            {
+              name: "tag",
+              hl_group: "Tag",
+              col: 1,
+              width: byteLength(action.tag),
+            },
+          ];
+          break;
+        }
+        default:
+          break;
+      }
+
+      return item;
+    }));
+  }
+
+  params(): Params {
+    return {};
+  }
+}
--- denops/@ddu-sources/git_ref.ts
+++ denops/@ddu-sources/git_ref.ts
@@ -90,14 +90,6 @@ export class Source extends BaseSource<Params> {
               display: branch,
               kind: "git_branch",
               action: { branch: branch, isRemote: true },
-              highlights: [
-                {
-                  name: "remote",
-                  hl_group: "Identifier",
-                  col: 1,
-                  width: branch.length,
-                },
-              ],
             });
 
             return;
@@ -110,9 +102,6 @@ export class Source extends BaseSource<Params> {
               display: tag,
               kind: "git_tag",
               action: { tag: tag },
-              highlights: [
-                { name: "tag", hl_group: "Tag", col: 1, width: tag.length },
-              ],
             });
 
             return;
