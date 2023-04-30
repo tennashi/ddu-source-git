f681d7489e821e1dbce99f9a7777c7977f026145
tennashi <tennashio@gmail.com>
2023-04-30 15:33:15 +0900

    iRemote status is indicated by icons.

--- denops/@ddu-filters/git_ref_set_remote_state.ts
+++ denops/@ddu-filters/git_ref_set_remote_state.ts
@@ -4,6 +4,7 @@ import {
 } from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
 import { FilterArguments } from "https://deno.land/x/ddu_vim@v2.5.0/base/filter.ts";
 import { ActionData as GitBranchActionData } from "../@ddu-kinds/git_branch.ts";
+import { State } from "../ddu-source-git/cache/git_remote/main.ts";
 
 type Params = Record<never, never>;
 
@@ -12,6 +13,19 @@ const decoder = new TextDecoder();
 
 function byteLength(input: string): number {
   return encoder.encode(input).length;
+}
+
+function remoteStateMarker(state: State): string {
+  switch (state) {
+    case "pushable":
+      return "↑";
+    case "pullable":
+      return "↓";
+    case "pushableAndPullable":
+      return "↑↓";
+    case "equal":
+      return "=";
+  }
 }
 
 export class Filter extends BaseFilter<Params> {
@@ -29,7 +43,9 @@ export class Filter extends BaseFilter<Params> {
               return item;
             }
 
-            item.display = `${item.display} [${action.remoteState}]`;
+            const stateMarker = remoteStateMarker(action.remoteState);
+
+            item.display = `${item.display} [${stateMarker}]`;
           }
           break;
         }
